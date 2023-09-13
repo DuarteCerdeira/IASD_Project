7a243301172c4550e586a35150bb446cdf12758c
DuarteCerdeira <duarte.cerdeira@tecnico.ulisboa.pt>
2023-09-13 18:19:24 +0100

    Added template class to solution file

---
+++ solution.py
@@ -0,0 +1,10 @@
+import search
+
+class FleetProblem(search.Problem):
+    def load(self, fh):
+        """Loads a problem from the opened file object fh."""
+        pass
+
+    def cost(self, sol):
+        """Compute cost of solution sol."""
+        pass
